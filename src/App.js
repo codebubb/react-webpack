b2e34deff6fde120c1c0f256a35ff7cfaa9408a0
James Bubb <jpbubb82@gmail.com>
2021-05-17 10:34:27 +0100

    Setup:

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function App() {
+    return (
+        <div>
+            <h1>React App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos delectus voluptatem ab repellat saepe eos molestiae cupiditate, omnis doloremque, necessitatibus totam consectetur porro sint quam libero aperiam? Laborum, quaerat?</p>
+        </div>
+    );
+}
